22bdbfe151e3292d40ae1644ba389a8f3ca42261
ArseniiSilaev <Senyasilaev@yandex.ru>
2022-01-23 13:22:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            while (true)
+            {
+                string newUserString = Console.ReadLine();
+                if (newUserString == "exit") 
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
